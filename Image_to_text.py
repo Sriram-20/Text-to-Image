aed2e5441af23f01f5de54f4a9626aaf7b5fdcdd
Sriram <64577383+Sriram-20@users.noreply.github.com>
2025-04-20 12:22:07 +0530

    Update Image_to_text.py

--- Image_to_text.py
+++ Image_to_text.py
@@ -6,7 +6,7 @@ from diffusers import StableDiffusionPipeline
 import torch
 
 #load_dotenv()
-openai.api_key = "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"
+openai.api_key = "xxxxx"
 
 #function to generate AI based images using OpenAI Dall-E
 def generate_images_using_openai(text):
